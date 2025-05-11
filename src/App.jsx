029d056ce3afd3af8a8e5bbe960ae7836473457b
Kiudd <alyssasmadja@gmail.com>
2025-05-11 14:50:53 +0200

    title changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+      <h1>Coucou tonton</h1>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Yahpadesushi ajout de Kriss</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 3)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR alyssa s
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more a
-      </p>
-    </>
-  );
-}
 
 export default App;
