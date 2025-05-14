3fb3c6187da46e5b9dcf10ab4ffaccd44da98c97
Kiudd <alyssasmadja@gmail.com>
2025-05-14 09:52:56 +0200

    Header changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,21 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header-container">
-      <div className="logo-container">
-        <img src="https://yahpadsushi.com/images/logo/logo.webp?v=1.06" alt="Yahpad Sushi" className="logo" />
+      <div className="logo-and-buttons">
+        <div className="logo-container">
+          <img
+            src="https://yahpadsushi.com/images/logo/logo.webp?v=1.06"
+            alt="Yahpad Sushi"
+            className="logo"
+          />
+        </div>
+        <div className="button-group">
+          <Button title="Accueil" />
+          <Button title="Mon menu" />
+          <Button title="Mon histoire" />
+          <Button title="Commander" />
+        </div>
       </div>
-
-      <Button title="Accueil" />
-      <Button title="isis" />
-      <Button title="alyssa" />
-      <Button title="cleo" />
     </div>
   );
 }
